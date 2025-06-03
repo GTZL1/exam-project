d76f6f1fc1d7ef7b00d40505ca81a0ecab2a8d81
David Dupraz <duprazdavid@gmail.com>
2025-06-03 15:04:52 +0200

    Create both selects

--- src/home/mainPage.js
+++ src/home/mainPage.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import ENDPOINTS from "../constants/endpoints";
 import Category from "./category.js";
+import Select from 'react-select';
+import { DIFFICULTIES } from "../constants/constants.js";
 
 export default function MainPage() {
     const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [difficulty, setDifficulty] = useState(null);
 
     useEffect(() => {
         axios
@@ -18,8 +22,41 @@ export default function MainPage() {
             });
     }, []);
 
-   
+    const handleCategoryChange = (newCat) => {
+        setSelectedCategory(new Category(newCat.value, newCat.label));
+    };
+
+    const handleDifficultyChange = (newDiff) => {
+        setDifficulty(newDiff.label);
+    };
+
     return (<>
         <h2>Quizz Maker</h2>
+        <SelectList
+            options={categories.map((a) => ({value: a.getId(), label: a.getName()}))} 
+            currentValue={selectedCategory !== null ? selectedCategory.getName() : null}
+            onChangeHandler={handleCategoryChange}
+            placeholder='Select a category'
+            id = 'categorySelect' />
+        <SelectList
+            options={DIFFICULTIES.map((d) => ({value: d, label: d}))}
+            currentValue={difficulty}
+            onChangeHandler={handleDifficultyChange}
+            placeholder='Select difficulty'
+            id =' difficultySelect' />
     </>);
+}
+
+function SelectList ({options, currentValue, onChangeHandler, placeholder, id}) {
+    return <Select
+        {...(currentValue !== null ?
+            {value: ({value : currentValue, label : currentValue})} : {})}
+        id = {id}
+        options={options}
+        onChange={onChangeHandler}
+        placeholder = {placeholder}
+        required="true"
+        className="basic-select"
+        classNamePrefix="select"
+        menuPortalTarget={document.body} />
 }
